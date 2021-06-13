61c9b7099592e13ae3ba6f299113e8e6a0f6af5b
haku-noir <hakunoir.00@gmail.com>
2021-06-13 15:19:00 +0900

    change: create page layout

--- src/resources/views/posts/create.blade.php
+++ src/resources/views/posts/create.blade.php
@@ -3,22 +3,32 @@
 <head>
     <meta charset="UTF-8" />
     <title>Laravel Sample Blog</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
 </head>
 <body>
-    <div>
-        <h1 class="bg-success"><a href="/">Laravel Sample Blog</a></h1>
+    <header class="hero is-primary">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title"><a href="{{ route('post.list') }}" class="has-textwhite">Laravelサンプルブログ</a></h1>
+                <h2 class="subtitle">サンプルアプリケーションです。</h2>
+            </div>
+        </div>
+    </header>
+    <div class="section">
+        <div class="container">
+            <h1 class="title">新規投稿</h1>
+            <form method="POST" action="/blog/create/">
+                @csrf
+                <b>タイトル</b><br>
+                <input name="title" value="" placeholder="タイトルを⼊⼒してください。"><br><br>
+
+                <b>本⽂</b><br>
+                <textarea cols="50" rows="15" name="content" placeholder="本⽂を⼊⼒してください。"></textarea><br>
+
+                <input type="submit" class="button is-primary" value="送信">
+                <a class="button is-primary" href="{{ route('post.list') }}"> キャンセル</a>
+            </form>
+        </div>
     </div>
-    <h2>新規投稿</h2>
-    <form method="POST" action="/blog/create/">
-        @csrf
-        タイトル<br>
-        <input name="title" value="" placeholder="タイトルを⼊⼒してください。"><br><br>
-
-        本⽂<br>
-        <textarea cols="50" rows="15" name="content" placeholder="本⽂を⼊⼒してください。"></textarea><br>
-
-        <input type="submit" class="btn btn-primary btn-sm" value="送信">
-    </form>
 </body>
 </html>
